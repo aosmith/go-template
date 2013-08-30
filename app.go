9014bfb4e0c056c4edb0c4c630fc63d584a310c2
Alex Smith <aosmith@gmail.com>
2013-08-30 01:27:51 -0500

    initial commit

---
+++ app.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "lib/config"
+ // "launchpad.net/goyaml"
+)
+
+
+
+func main() {
+  fmt.Println("Booting server...")
+  
+}
+
+
+func load_config_file(path string) {
+
+}
