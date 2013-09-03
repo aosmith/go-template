3a2bd27c0659c449441bfe970dca6baab7211d2c
Alex Smith <aosmith@gmail.com>
2013-09-03 14:50:05 -0500

    simplified router

--- src/router/router.go
+++ src/router/router.go
@@ -1,18 +1,13 @@
 package router
 
 import (
+  "fmt"
   "net/http"
 )
 
 
+
+
 func ApplicationRouter(writer http.ResponseWriter, request *http.Request) {
-  if request.Method == "GET" {
-    // This is a request for a resource
-  } else if request.Method == "POST" {
-    // This is a request to create a resource
-  } else if request.Method == "PUT" {
-    // This is a request to update a resource
-  } else if request.Method == "DELETE" {
-    // This is a request to delete a resource
-  }
+  fmt.Println("Booting router...")
 }
